8d1b8283df277846ed63526a96daa0e8bd54e99c
Si Wei <swei@colgate.edu>
2019-11-19 14:26:09 -0500

    Finish runExperiment

--- lab9/BloomFilter.java
+++ lab9/BloomFilter.java
@@ -1,14 +1,18 @@
+import java.util.*;
 /**
  * Colgate University COSC 290 Labs
  * Version 0.2,  2019
  *
  * @author Michael Hay
  */
+
+
 public class BloomFilter<E> {
 
     private final int k;  // number of hash functions
     private final int m;  // size of hash array
     private HashFamily<E> hf;  // hash family from which hash functions are obtained
+    private boolean[] bloomFilterArray;
 
     /**
      * Makes a new bloom filter with m slots and k hash functions from the specified hash family.
@@ -20,14 +24,27 @@ public class BloomFilter<E> {
         this.k = k;
         this.m = m;
         // you may want to initialize some private fields here
+        this.hf = hf;
+        bloomFilterArray = new boolean[m];
     }
 
     public boolean lookUp(E item) {
-        throw new UnsupportedOperationException("implement me!");
+        for (int i =0; i<k; i++) {
+          // System.out.println(hf.hash(item,i));
+          // System.out.println(bloomFilterArray.length);
+          if (!(bloomFilterArray[hf.hash(item, i)])) {
+            return false;
+          }
+        }
+        return true;
     }
 
     public void insert(E item) {
-        throw new UnsupportedOperationException("implement me!");
+        for (int i = 0; i < k; i++ ) {
+          if ((bloomFilterArray[hf.hash(item, i)]) != true) {
+            bloomFilterArray[hf.hash(item, i)] = true;
+          }
+        }
     }
 
     /**
@@ -46,7 +63,29 @@ public class BloomFilter<E> {
      * @return the false positive rate
      */
     public static double runExperiment() {
-        throw new UnsupportedOperationException("implement me!");
+
+        double rate;
+        double numTrue  = 0.0;
+        double numAdded = 0.0;
+
+        HashFamily<String> hf = new HashFamilySmart<String>(1000000, 7);
+
+        BloomFilter<String> bf = new BloomFilter<String>(1000000, 7, hf);
+
+        for(int i =0;i<200000;i+=2){
+          bf.insert(Integer.toString(i));
+        }
+
+        for(int i =1;i<200000;i+=2){
+            if (bf.lookUp(Integer.toString(i)) == true) {
+              numTrue ++;
+            }
+            numAdded ++;
+        }
+
+        rate = (numTrue/numAdded);
+        return rate;
+
     }
 
     public static void main(String[] args) {
@@ -61,6 +100,8 @@ public class BloomFilter<E> {
         bf.insert(2);  // slots 2 and 3 will be marked
         System.out.println("Lookup 2: " + bf.lookUp(2));
         System.out.println("Lookup 1: " + bf.lookUp(1));  // true because it checks slots 1 and 2, both marked
+
+        System.out.println("False positive rate: " + runExperiment());
     }
 
 }
