56c4c6a1f53361db6e4485ee94e63617b5e27d8e
Si Wei <swei@colgate.edu>
2019-12-02 12:58:22 -0500

    Initial commit for lab 10

---
+++ lab10/Warshall.java
@@ -0,0 +1,31 @@
+// Colgate University COSC 290 Labs
+// Version 0.1,  2017
+// Author: Michael Hay
+
+import java.util.NoSuchElementException;
+
+/**
+ * A class for computing transitive closure using Warshall's algorithm
+ */
+public class Warshall {
+
+    /**
+     * Returns transitive closure of R.  This method uses Warshall's algorithm.
+     * @param R relation, represented as a matrix (double array)
+     * @return the transitive closure of R
+     */
+    public static boolean[][] transitiveClosure(boolean[][] R) {
+        throw new UnsupportedOperationException("implement me!");
+    }
+
+
+    /**
+     * Compare the runtimes of the two methods for transitive closure.  {@see System#currentTimeMillis()}
+     * @param args
+     */
+    public static void main(String[] args) {
+         // test your implementation here!
+        throw new UnsupportedOperationException("implement me!");
+    }
+
+}
